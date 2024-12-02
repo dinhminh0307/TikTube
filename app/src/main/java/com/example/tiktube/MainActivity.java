cf877918ccffb78779ae067cbf86bc52df6950f1
dinhminh0307 <doanlamyomostt123@gmail.com>
2024-12-02 12:35:19 +0700

    feat: add the signup page ui, button

--- app/src/main/java/com/example/tiktube/MainActivity.java
+++ app/src/main/java/com/example/tiktube/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tiktube;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.tiktube.frontend.login.LoginActivity;
+import com.example.tiktube.frontend.register.RegisterActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,12 +21,37 @@ import com.example.tiktube.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button loginBtn;
+    Button signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onSignUpButtonClicked();
+        onLoginButtonClicked();
     }
 
+    private void onSignUpButtonClicked() {
+        signUpBtn = findViewById(R.id.signUpButton);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onLoginButtonClicked() {
+        loginBtn = findViewById(R.id.loginButton);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 }
