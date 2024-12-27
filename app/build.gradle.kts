333f8267941598615ee947cb847a255bc6b651c6
The Anh <s3927195@rmit.edu.vn>
2024-12-27 15:03:26 +0700

    Changed video page icons
    Switched old JPG icons for xml icons
    Minor adjustments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.activity)
+    implementation(libs.play.services.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
